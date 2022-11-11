a192871a20be223320038ba12d283abbf184d6a1
HaraT0323 <alra9si916323@gmail.com>
2022-11-11 22:19:35 +0900

    NextTokenメソッドをLexer構造体に追加

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/HT0323/monkey_interpreter/token"
+
 // 字句解析器(レキサー)の構造体
 type Lexer struct {
 	input        string // 入力値
@@ -27,4 +29,37 @@ func (l *Lexer) readChar() {
 	// カウンターをインクリメント
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+// 現在精査中の文字に対応したトークンを生成
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+// Token構造体の初期化
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
