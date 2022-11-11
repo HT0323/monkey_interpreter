c4ca9e40084aa9f388cf6259ad94c486aa24d689
HaraT0323 <alra9si916323@gmail.com>
2022-11-11 22:04:33 +0900

    readCharメソッドをLexer構造体に追加

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,5 +11,20 @@ type Lexer struct {
 // Lexer構造体の初期化
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+// 次の文字を取得
+func (l *Lexer) readChar() {
+	//入力が終端に達しているかを確認
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		// 終端に達していなければ次の文字を取得
+		l.ch = l.input[l.readPosition]
+	}
+	// カウンターをインクリメント
+	l.position = l.readPosition
+	l.readPosition += 1
 }
