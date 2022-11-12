8ee147fc18500d5dc781b819457ee0df0a6065fa
HaraT0323 <alra9si916323@gmail.com>
2022-11-12 17:01:06 +0900

    let文の構文解析処理を追加

--- parser/parser.go
+++ parser/parser.go
@@ -28,6 +28,69 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+// tokenを読み進めてNodeのツリーを作成する
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+// tokenTypeに対応するNode構築メソッドの呼び出し
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+// LetStatement Nodeの構築
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// letの次のtokenTypeが変数名であることを期待
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// 変数名の次のtokenTypeが=であることを期待
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// 現在のトークンのトークンタイプが渡されたトークンタイプと一致するかチェック
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// 次のトークンのトークンタイプが渡されたトークンタイプと一致するかチェック
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// 次のトークンが期待値のTokenTypeと一致するか判定し、一致したら次のトークンを読み進める
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
