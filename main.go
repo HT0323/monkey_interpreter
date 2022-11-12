d6aaf89c67f62eb0058366359ec3e2c68a342d36
HaraT0323 <alra9si916323@gmail.com>
2022-11-12 10:21:13 +0900

    repl実行できるように修正

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/HT0323/monkey_interpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming Language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
