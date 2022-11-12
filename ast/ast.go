e8616d5bf12f360098a9d0b224972a719916bcb6
HaraT0323 <alra9si916323@gmail.com>
2022-11-12 11:13:09 +0900

    変数定義の部分の構造体の実装

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"github.com/HT0323/monkey_interpreter/token"
+)
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -13,3 +17,34 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type Program struct {
+	Statements []Statement
+}
+
+// Nodeに紐づいているトークンのリテラル値を返却
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier //変数名
+	Value Expression  //値
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// 変数名・識別子の構造体
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
