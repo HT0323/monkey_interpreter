d5e516853a03a91847ad2a0688ffbca48c22b13d
HaraT0323 <alra9si916323@gmail.com>
2022-11-22 22:12:08 +0900

    真偽値の構造体の定義

--- ast/ast.go
+++ ast/ast.go
@@ -159,3 +159,15 @@ func (oe *InfixExpression) String() string {
 
 	return out.String()
 }
+
+// 真偽値の構造体
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
