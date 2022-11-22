54afa3e568dd5bc555bc3d49e433bc8e390e02cf
HaraT0323 <alra9si916323@gmail.com>
2022-11-22 20:10:42 +0900

    中間演算子の構文解析処理を追加

--- parser/parser.go
+++ parser/parser.go
@@ -9,6 +9,7 @@ import (
 	"github.com/HT0323/monkey_interpreter/token"
 )
 
+// 数値が大きくなるほど優先順位が高くなる
 const (
 	_ int = iota
 	LOWSET
@@ -19,6 +20,17 @@ const (
 	PREFIX      // -, !
 	CALL        // myFunction(X)
 )
+
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
 
 type Parser struct {
 	l      *lexer.Lexer
@@ -48,6 +60,16 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+
+	p.infixParseFns = make(map[token.TokenType]infixParseFn)
+	p.registerInfix(token.PLUS, p.parseInfixExpression)
+	p.registerInfix(token.MINUS, p.parseInfixExpression)
+	p.registerInfix(token.SLASH, p.parseInfixExpression)
+	p.registerInfix(token.ASTERISK, p.parseInfixExpression)
+	p.registerInfix(token.EQ, p.parseInfixExpression)
+	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
+	p.registerInfix(token.LT, p.parseInfixExpression)
+	p.registerInfix(token.GT, p.parseInfixExpression)
 
 	return p
 }
@@ -181,6 +203,17 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 		return nil
 	}
 	leftExp := prefix()
+
+	// for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+	// 	infix := p.infixParseFns[p.peekToken.Type]
+	// 	if infix == nil {
+	// 		return leftExp
+	// 	}
+
+	// 	p.nextToken()
+
+	// 	leftExp = infix(leftExp)
+	// }
 	return leftExp
 }
 
@@ -215,6 +248,38 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 	p.nextToken()
 
 	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
+}
+
+// 次のトークンタイプに対応する優先順位を取得
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+
+	return LOWSET
+}
+
+// 現在のトークンタイプに対応する優先順位を取得
+func (p *Parser) curPrecedence() int {
+	if p, ok := precedences[p.curToken.Type]; ok {
+		return p
+	}
+
+	return LOWSET
+}
+
+// 中間演算子のASTノードを作成
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	expression := &ast.InfixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+		Left:     left,
+	}
+	precedence := p.curPrecedence()
+	p.nextToken()
+	expression.Right = p.parseExpression(precedence)
 
 	return expression
 }
