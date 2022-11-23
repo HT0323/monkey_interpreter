f11bd25cc72e0125d4bfc785764cab293bbc9388
HaraT0323 <alra9si916323@gmail.com>
2022-11-23 20:01:21 +0900

    REPLの修正

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/HT0323/monkey_interpreter/evaluator"
 	"github.com/HT0323/monkey_interpreter/lexer"
 	"github.com/HT0323/monkey_interpreter/parser"
 )
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
