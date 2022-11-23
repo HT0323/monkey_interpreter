3b912bb03e817ad71127b59b2c5d5bf9491e23d6
HaraT0323 <alra9si916323@gmail.com>
2022-11-23 19:58:02 +0900

    整数型Object生成の処理を追加

---
+++ evaluator/evaluator.go
@@ -0,0 +1,32 @@
+package evaluator
+
+import (
+	"github.com/HT0323/monkey_interpreter/ast"
+	"github.com/HT0323/monkey_interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// 文
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	// 式
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+// statement構造体を配列から取り出しObjectを作成する
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
