0b8392a736fd744bdcb05cd0fbf1aa6dcf1cdd61
HaraT0323 <alra9si916323@gmail.com>
2022-11-23 18:36:21 +0900

    nullを表現するオブジェクトを追加

--- object/object.go
+++ object/object.go
@@ -7,6 +7,7 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -22,7 +23,6 @@ type Integer struct {
 func (i *Integer) Inspect() string {
 	return fmt.Sprintf("%d", i.Value)
 }
-
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
 // 真偽値を表現するオブジェクト
@@ -33,5 +33,10 @@ type Boolean struct {
 func (b *Boolean) Inspect() string {
 	return fmt.Sprintf("%t", b.Value)
 }
-
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+// nullを表現するオブジェクト
+type Null struct{}
+
+func (n *Null) Inspect() string  { return "null" }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
