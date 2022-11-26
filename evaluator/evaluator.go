d81cd9ec0f55a27508a284243192caf90bbcdb0f
HaraT0323 <alra9si916323@gmail.com>
2022-11-26 23:48:02 +0900

    環境構造体の修正

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -62,6 +62,15 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		params := node.Parameters
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
 	}
 
 	return nil
@@ -240,4 +249,20 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 	}
 
 	return val
+}
+
+//引数の各要素を評価する (2 + 2, 5 * 3) -> (4, 15)
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+
+		result = append(result, evaluated)
+	}
+
+	return result
 }
--- object/environment.go
+++ object/environment.go
@@ -1,16 +1,27 @@
 package object
 
+type Environment struct {
+	store map[string]Object
+	outer *Environment //  新たにEnvironment構造体を作成するとる時に既存の構造体を格納する
+}
+
 func NewEnviroment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
 }
 
-type Environment struct {
-	store map[string]Object
+//既存のEnvironment構造体を引き継いで新たに構造体を作成
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnviroment()
+	env.outer = outer
+	return env
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
