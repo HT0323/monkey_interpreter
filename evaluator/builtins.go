d7f1dc452b45b2fbed83e2bd18b340cd5ee49ff8
HaraT0323 <alra9si916323@gmail.com>
2022-12-02 20:31:32 +0900

    自働テスト導入

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -7,24 +7,24 @@ import (
 
 //組み込み関数名とそれに対応する処理を保持
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{Fn: func(args ...object.Object) object.Object {
-		//len関数の引数は一つだけなのでそれ以外の場合はエラー
-		if len(args) != 1 {
-			return newError("wrong number of arguments. got=%d, want=1",
-				len(args))
-		}
-		switch arg := args[0].(type) {
-		case *object.Array:
-			return &object.Integer{Value: int64(len(arg.Elements))}
-		case *object.String:
-			return &object.Integer{Value: int64(len(arg.Value))}
-		default:
-			//有効な引数はString型のみ
-			return newError("argument to `len` not supported, got %s",
-				args[0].Type())
-		}
-	},
-	},
+	//"len": &object.Builtin{Fn: func(args ...object.Object) object.Object {
+	//	//len関数の引数は一つだけなのでそれ以外の場合はエラー
+	//	if len(args) != 1 {
+	//		return newError("wrong number of arguments. got=%d, want=1",
+	//			len(args))
+	//	}
+	//	switch arg := args[0].(type) {
+	//	case *object.Array:
+	//		return &object.Integer{Value: int64(len(arg.Elements))}
+	//	case *object.String:
+	//		return &object.Integer{Value: int64(len(arg.Value))}
+	//	default:
+	//		//有効な引数はString型のみ
+	//		return newError("argument to `len` not supported, got %s",
+	//			args[0].Type())
+	//	}
+	//},
+	//},
 	"puts": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
